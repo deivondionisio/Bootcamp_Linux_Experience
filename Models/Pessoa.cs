e20aec648f89a2cc4b988788f137f39f05cc1b1b
David Dionísio <david.dionisio@sapo.pt>
2022-09-15 21:44:58 -0300

    Criando nossa classe no código

---
+++ Models/Pessoa.cs
@@ -0,0 +1,20 @@
+using Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootcamp_Linux_Experience.Models
+{
+  public class Pessoa
+  {
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    public void Apresentar()
+
+    {
+      Console.WriteLine($"Olá, seu nome é {Nome}, e tenho {Idade} anos");
+    }
+  }
+}
