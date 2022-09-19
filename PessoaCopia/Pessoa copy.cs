cfe872ee5c516cdad708977612090af98780f6e8
David Dionísio <david.dionisio@sapo.pt>
2022-09-19 15:49:20 -0300

    Convenção de escrita de classe

--- PessoaCopia/Pessoa copy.cs
+++ PessoaCopia/Pessoa copy.cs
@@ -9,6 +9,7 @@ namespace Bootcamp_Linux_Experience.Models.PessoaCopia
   {
     public string Nome { get; set; }
     public int Idade { get; set; }
+    public string NomeRepresentanteLegalDaPessoaFisica { get; set; }
 
     public void Apresentar()
 
