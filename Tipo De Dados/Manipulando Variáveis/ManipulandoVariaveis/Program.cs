0dc5e49e9d7156c26bf9ead1888f30366039cbfd
David Dionísio <david.dionisio@sapo.pt>
2022-09-19 16:12:13 -0300

    Sintaxe e tipo de Dados em C#

---
+++ Tipo De Dados/Manipulando Variáveis/ManipulandoVariaveis/Program.cs
@@ -0,0 +1,7 @@
+﻿string apresentacao = "Olá, seja bem vindo";
+
+int quantidade = 1;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+quantidade = 10;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
