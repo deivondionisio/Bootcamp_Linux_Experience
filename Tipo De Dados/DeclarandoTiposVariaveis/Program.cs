fa355bc7f36610a2b9edc2b3d2bafda20afe739b
David Dionísio <david.dionisio@sapo.pt>
2022-09-19 16:04:45 -0300

    Organização/Tipo de Dados

---
+++ Tipo De Dados/DeclarandoTiposVariaveis/Program.cs
@@ -0,0 +1,15 @@
+﻿string apresentacao = "Olá, seja bem vindo";
+
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condição = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condição);
