c23972a2492e37e73732d7a686957398f0462622
David Dionísio <david.dionisio@sapo.pt>
2022-09-20 15:44:50 -0300

    Operador AND na Prática

---
+++ Tipos de Operadores em C#/Operadores Lógicos/OperadorAnd/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using Internal;
+using System;
+bool possuiPresencaMinima = true;
+double media = 5.5;
+
+if (possuiPresencaMinima && media >= 7)
+{
+  Console.WriteLine("Aprovado!");
+}
+else
+{
+  Console.WriteLine("Reprovado!");
+}
