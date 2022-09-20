c5d7add0f897b68528ae8853c0d7faa60f3a4e1a
David Dionísio <david.dionisio@sapo.pt>
2022-09-20 00:15:39 -0300

    Operador condicional na prática, verdadeiro/falso

---
+++ Tipos de Operadores em C#/Operadores Condicionais/OperadorCondicionalNaPratica/Program.cs
@@ -0,0 +1,16 @@
+﻿int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque : {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if (possivelVenda)
+{
+  Console.WriteLine("Venda realizada.");
+}
+else
+{
+  Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+}
