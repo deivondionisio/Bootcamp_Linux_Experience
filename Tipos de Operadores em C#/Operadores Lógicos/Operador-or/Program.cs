877481edabeb4f7b08dbc3d16057ad4eb0b760c6
David Dionísio <david.dionisio@sapo.pt>
2022-09-20 14:13:29 -0300

    Operador OR na prática

---
+++ Tipos de Operadores em C#/Operadores Lógicos/Operador-or/Program.cs
@@ -0,0 +1,11 @@
+﻿bool ehMaiorDeIdade = false;
+bool possuiAutorizacaoDoResponsavel = false;
+
+if (ehMaiorDeIdade || possuiAutorizacaoDoResponsavel)
+{
+  Console.WriteLine("Entrada Liberada!");
+}
+else
+{
+  Console.WriteLine("Entrada não liberada");
+}
