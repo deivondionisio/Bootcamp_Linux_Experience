a2201c6be688073ebb795514a3856d3b9630b6e1
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2022-09-21 20:08:56 -0300

    Introdução ao While

---
+++ Criando a nossa classe Calculadora/PessoaCopia/Pessoa copy.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootcamp_Linux_Experience.Models.PessoaCopia
+{
+  public class Pessoa
+  {
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string NomeRepresentanteLegalDaPessoaFisica { get; set; }
+
+    public void Apresentar()
+
+    {
+      Console.WriteLine($"Olá, seu nome é {Nome}, e tenho {Idade} anos");
+    }
+  }
+}
