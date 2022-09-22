9bb12da613764f63a5b32c744cb509ae215b4b43
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2022-09-21 21:54:50 -0300

    Interrompendo o fluxo de execução

---
+++ Conhecendo as Estruturas de Repetição em C#/InterrompendoFluxoExecução/Program.cs
@@ -0,0 +1,13 @@
+﻿int numero = 5;
+int contador = 1;
+
+while (contador <= 10)
+{
+    Console.WriteLine($"{contador}° Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+
+    if (contador == 6)
+    {
+        break;
+    }
+}
