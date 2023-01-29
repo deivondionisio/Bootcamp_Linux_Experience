4a9b9a7d3a8aa2be20a795b31c6da587620a15ed
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-01-29 14:18:45 -0300

    Variação no Get e Set

---
+++ Propriedades, métodos e construtores/EXEMPLOEXPLORANDO/Models/Pessoa.cs
@@ -0,0 +1,32 @@
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                return _nome.ToUpper();
+            }
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+            }
+        }
+
+
+        public int Idade { get; set; }
+
+        public void apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
+    }
+}
---
+++ Propriedades, métodos e construtores/EXEMPLOEXPLORANDO/Program.cs
@@ -0,0 +1,7 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "David";
+p1.Idade = 32;
+p1.apresentar();
