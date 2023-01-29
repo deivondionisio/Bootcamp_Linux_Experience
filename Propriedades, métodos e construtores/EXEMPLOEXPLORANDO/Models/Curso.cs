78d296fab114dbba0587bdd753c2e3f68a0e6d87
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-01-29 17:56:28 -0300

    Implementando a classe curso

---
+++ Propriedades, métodos e construtores/EXEMPLOEXPLORANDO/Models/Curso.cs
@@ -0,0 +1,13 @@
+namespace ExemploExplorando.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+    }
+}
