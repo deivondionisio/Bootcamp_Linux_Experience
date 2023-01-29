e327b5032ef403659eec9426b345bf726f38ea09
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-01-29 17:24:40 -0300

    Validando a idade

--- Propriedades, métodos e construtores/EXEMPLOEXPLORANDO/Models/Pessoa.cs
+++ Propriedades, métodos e construtores/EXEMPLOEXPLORANDO/Models/Pessoa.cs
@@ -3,6 +3,7 @@ namespace ExemploExplorando.Models
     public class Pessoa
     {
         private string _nome;
+        private int _idade;
         public string Nome
         {
             get => _nome.ToUpper();
@@ -20,7 +21,20 @@ namespace ExemploExplorando.Models
         }
 
 
-        public int Idade { get; set; }
+        public int Idade
+        {
+            get => _idade;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que zero!");
+                }
+
+                _idade = value;
+            }
+        }
 
         public void apresentar()
         {
--- Propriedades, métodos e construtores/EXEMPLOEXPLORANDO/Program.cs
+++ Propriedades, métodos e construtores/EXEMPLOEXPLORANDO/Program.cs
@@ -3,5 +3,5 @@
 Pessoa p1 = new Pessoa();
 
 p1.Nome = "David";
-p1.Idade = 32;
+p1.Idade = -32;
 p1.apresentar();
