295b6e12f77be355a9a534fa5db3c015cbe744b2
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-08-27 13:15:12 +0000

    Implementando método Listar Alunos

--- Propriedades, métodos e construtores/EXEMPLOEXPLORANDO/Program.cs
+++ Propriedades, métodos e construtores/EXEMPLOEXPLORANDO/Program.cs
@@ -6,3 +6,24 @@ p1.Nome = "David";
 p1.Sobrenome = "Dionisio";
 p1.Idade = 32;
 p1.apresentar();
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Anderson";
+p2.Sobrenome = "Dionisio";
+p2.Idade = 45;
+p2.apresentar();
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+//Pessoa p1 = new Pessoa();
+
+//p1.Nome = "David";
+//p1.Sobrenome = "Dionisio";
+//p1.Idade = 32;
+//p1.apresentar();
